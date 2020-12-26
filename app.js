6671f26d41c645ba618dff6a95e126d6ee2054f9
ahmedhabeeb73 <alnaib888@gmail.com>
2020-12-26 14:42:34 +0100

    sarch clear

--- app.js
+++ app.js
@@ -1,32 +1,78 @@
 const auth="563492ad6f917000010000017aa97a8c8674436d8da997ea725c90e6";
 const gallery=document.querySelector('.gallery');
 const searchInput=document.querySelector('.search-input');
-const submitButton= document.querySelector('.submit-btn');
+const form= document.querySelector('.search-form');
 let searchValue;
 
 
-async function curatedPhotos()
-{
-  const dataFetch=await fetch("https://api.pexels.com/v1/curated?per_page=15",{
+searchInput.addEventListener('input',updateInput);
 
-  method:'GET',
-  headers:{
-    Accept:'application/json',
-    Authorization:auth
-  }
+form.addEventListener('submit',(e)=>{
+  e.preventDefault();
+  searchPhotos(searchValue)
+
+
+})
+
+
+function updateInput(e)
+{
+searchValue=e.target.value;
+}
+
+
+async function fetchApi(url)
+{
+
+  const dataFetch= await fetch(url,{
+    method:'GET',
+    headers:{
+      Accept:'application/json',
+      Authorization:auth
+    }
   })
 
   const data=await dataFetch.json();
+  return data;
+
+}
+
+
+function html(data) {
   data.photos.forEach(photo =>{
     const galleryImg=document.createElement('div');
     galleryImg.classList.add('gallery-img');
-    galleryImg.innerHTML=`<img src=${photo.src.large}></img>
+    galleryImg.innerHTML=`
+    <div class="gallery-info">
     <p>${photo.photographer}</p>
+    <a href=${photo.src.original}>Download</a>
+    </div>
+    <img src=${photo.src.large}></img>
     `;
     gallery.appendChild(galleryImg)
 
   });
 }
 
+
+async function curatedPhotos()
+{
+  const data=await fetchApi("https://api.pexels.com/v1/curated?per_page=15")
+  html(data);
+}
+
+
+async function searchPhotos(query)
+{
+  clear();
+  const data=await fetchApi(`https://api.pexels.com/v1/search?query=${query}+query&per_page=15&page=1`)
+   html(data);
+}
+
+
+function  clear() {
+  gallery.innerHTML='';
+  searchInput.value='';
+}
 
 curatedPhotos();
